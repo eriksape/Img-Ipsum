a279ea3b1e686e7983b872bd47d402b02a8497c9
Erik R Sánchez Pérez <erik.sape@gmail.com>
2016-05-04 15:17:55 -0500

    first commit

---
+++ app/Http/routes.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+  $img = Image::make('2833540510_23b8d43865_o.jpg')->fit(1920, 1920);
+  return $img->response('jpg');
+});
+
+Route::get('/generate', 'CustomImageCacheController@generate');
+
+Route::get('/generate/{width}/{height}/{category}', 'CustomImageCacheController@prueba');
