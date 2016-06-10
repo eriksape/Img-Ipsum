8583fde6fa37b09ccbb29e5e1cf3e5bbefc2a70c
Erik Sanchez <erik.sape@gmail.com>
2016-06-10 10:44:05 -0500

    structuring semantic

--- resources/assets/js/application.js
+++ resources/assets/js/application.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './components/App.jsx'
-require('./../semantic/src/semantic.less');
+require('./../semantic/semantic.less');
 render(
   <App /> ,
   document.getElementById('application')
