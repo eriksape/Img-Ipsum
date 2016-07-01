899c226e699a069db434a37911dd00568465d6bd
Erik R Sánchez Pérez <erik.sape@gmail.com>
2016-07-01 06:22:17 -0500

    update reducer

--- resources/assets/js/reducers/authors/authorReducer.js
+++ resources/assets/js/reducers/authors/authorReducer.js
@@ -10,10 +10,17 @@ const { success, fail } = raspberry.constants
 
 export default (state = initialState, action)=>{
   const { type, promise, payload } = action
+  let index = -1;
   switch (type) {
     case success.index:
       return state.merge(payload.value)
       break
+    case success.update:
+      index = state.get('data').findIndex( d => d.get('id')==payload.value.id )
+      debugger
+      if(index >= 0)
+        return state.mergeIn(['data', index], payload.value)
+      else return state
     case success.show:
     case 'authors_SET_FORM':
       return state.merge({
@@ -27,6 +34,7 @@ export default (state = initialState, action)=>{
       break
     case fail.index:
     case fail.update:
+    debugger
       promise.reject(new SubmissionError( payload.value ))
     default:
       return state
